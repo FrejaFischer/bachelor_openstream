801048b3402d51d4b14c4e3fa0bc5d77d537af1d
Louis Nordsø <ln@magenta-aps.dk>
2025-09-23 20:51:46 +0200

    fix translations

--- frontend/src/assets/utils/locales.js
+++ frontend/src/assets/utils/locales.js
@@ -57,12 +57,12 @@ export function gettext(key) {
   if (lang === "en") return key;
 
   if (!translations[key]) {
-    //console.error(`Translation not found for key: "${key}"`);
+    console.error(`Translation not found for key: "${key}"`);
     return key;
   }
   const translated = translations[key][lang];
   if (!translated) {
-    //console.error(`Translation not found for key: "${key}" in language: "${lang}"`);
+    console.error(`Translation not found for key: "${key}" in language: "${lang}"`);
     return key;
   }
   return translated;
