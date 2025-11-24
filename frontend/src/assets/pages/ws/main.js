3cfe6b8a58fb1ec0eb7fadc61e084ae74f59bc5a
Freja Fischer <frej657e@stud.kea.dk>
2025-11-24 13:33:30 +0100

    implemented first simple WS endpoint in backend, with routing, consumer and frontend testing (POC)

---
+++ frontend/src/assets/pages/ws/main.js
@@ -0,0 +1,39 @@
+// import { BASE_URL } from "../../utils/constants";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  //   const roomName = JSON.parse(document.getElementById("room-name").textContent);
+  const roomName = "room1";
+
+  const chatSocket = new WebSocket("ws://localhost:8000" + "/ws/chat/" + roomName + "/");
+  // ${BASE_URL}/ws/chat/{roomName}/ (BASE_URL = http://) - TO DO: Make WS BASE_URL version?
+  // ws://localhost:8000/ws/chat/{roomName}/
+
+  chatSocket.onmessage = function (e) {
+    console.log("onMessage", e);
+    const data = JSON.parse(e.data);
+    document.querySelector("#chat-log").value += data.message + "\n";
+  };
+
+  chatSocket.onclose = function (e) {
+    console.error("Chat socket closed unexpectedly");
+  };
+
+  document.querySelector("#chat-message-input").focus();
+  document.querySelector("#chat-message-input").onkeyup = function (e) {
+    if (e.key === "Enter") {
+      // enter, return
+      document.querySelector("#chat-message-submit").click();
+    }
+  };
+
+  document.querySelector("#chat-message-submit").onclick = function (e) {
+    const messageInputDom = document.querySelector("#chat-message-input");
+    const message = messageInputDom.value;
+    chatSocket.send(
+      JSON.stringify({
+        message: message,
+      })
+    );
+    messageInputDom.value = "";
+  };
+});
