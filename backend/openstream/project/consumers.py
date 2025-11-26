58f12a7e978f71507e03842b2d8b5e10174db114
Freja Fischer <frej657e@stud.kea.dk>
2025-11-26 14:47:09 +0100

    Implemented helper functions for GET and PATCH slideshow + used them in WS consumer

--- backend/openstream/project/consumers.py
+++ backend/openstream/project/consumers.py
@@ -2,7 +2,13 @@ import json
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from django.db import close_old_connections
+from django.shortcuts import get_object_or_404
 # from channels.generic.websocket import WebsocketConsumer # Synchronous solution
+
+from app.models import Slideshow
+from app.serializers import SlideshowSerializer
 
 # This is my first consumer, which handles a basic chat WS connection
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -15,6 +21,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
+        # Get current slideshow data by id
+        self.slideshow = await get_slideshow(self, 2)
+        # print("Connect", self.slideshow["slideshow_data"]["slides"][0]["name"])
+        print("consumer has the data: ", self.slideshow)
+
         # Accept the WebSocket connection (if the user needs to be authenticated, then it should happen before this, so the connection can be rejected)
         await self.accept()
 
@@ -24,21 +35,93 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     # Receive message from the WebSocket (message sent from the user)
     async def receive(self, text_data):
+        print("receive")
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
         # Send message to the whole room group
         await self.channel_layer.group_send(self.room_group_name, {"type": "chat.message", "message": message})
         # type indicates which method should be used to receive the event. type: chat.message can be received with chat_message (dot . is being replaced with _)
+
+        # Update the database with data from the user
+        updated_slideshow_data = text_data_json["data"]
+        print(updated_slideshow_data)
+        updated_slideshow = await patch_slideshow(self, updated_slideshow_data, 2)
+        print("updated_slideshow", updated_slideshow)
+        # send new slideshow data to the whole room group
+        await self.channel_layer.group_send(self.room_group_name, {"type": "chat.slideshow", "data": updated_slideshow})
+        
+
     
-     # Receive message from the room group (messages from other users in the room)
+    # Receive message from the room group (messages from other users in the room)
     async def chat_message(self, event):
+        print("chat_message", event)
         message = event["message"]
 
         # Send message to the WebSocket
         await self.send(text_data=json.dumps({"message": message}))
 
+    # Receive data from the room group (data from other users in the room)
+    async def chat_slideshow(self, event):
+        print("chat_slideshow", event)
+        data = event["data"]
 
+        await self.send(text_data=json.dumps({"data": data}))
+
+###############################################################################
+# Database helper functions using Decorators
+###############################################################################
+
+@database_sync_to_async
+def get_slideshow(self, id):
+    """
+    Get slideshow by id from database, with Slideshow data included
+
+    :param id: The id of the current slideshow
+    """
+    print("getting slideshow")
+
+    # Close old DB connections before making new ORM operations
+    close_old_connections()
+
+    try:
+        # branch = get_branch_from_request(request)
+        branch = 15 # Test - will be coming from the request in future
+    except ValueError as e:
+            return {"detail": str(e)} 
+
+    slideshow_id = id
+    context = {"include_slideshow_data": "true"}
+
+    ss = get_object_or_404(Slideshow, pk=slideshow_id, branch=branch)
+    ser = SlideshowSerializer(ss, context=context)
+    return ser.data
+
+@database_sync_to_async
+def patch_slideshow(self, data, id):
+    """
+    Patch / update slideshow by id
+
+    :param data: The data to update
+    :param id: The id of the current slideshow
+    """
+    print("updating slideshow")
+
+    # Close old DB connections before making new ORM operations
+    close_old_connections()
+
+    try:
+        # branch = get_branch_from_request(request)
+        branch = 15 # Test - will be coming from the request in future
+    except ValueError as e:
+        return {"detail": str(e)}
+
+    slideshow = get_object_or_404(Slideshow, pk=id, branch=branch)
+    serializer = SlideshowSerializer(slideshow, data=data, partial=True)
+    if serializer.is_valid():
+        updated = serializer.save()
+        return SlideshowSerializer(updated).data
+    return serializer.errors
 
 ### First solution with the consumer being synchronous
 # class ChatConsumer(WebsocketConsumer): 
