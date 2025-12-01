b49e46fa7453d1084c91fa5bde7a5ef59e28b4be
Freja Fischer <frej657e@stud.kea.dk>
2025-12-01 15:10:59 +0100

    Implemented authentication handling - WIP

--- backend/openstream/project/consumers.py
+++ backend/openstream/project/consumers.py
@@ -1,4 +1,5 @@
 import json
+import asyncio
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
@@ -6,13 +7,26 @@ from channels.db import database_sync_to_async
 from django.db import close_old_connections
 from django.shortcuts import get_object_or_404
 # from channels.generic.websocket import WebsocketConsumer # Synchronous solution
+from rest_framework_simplejwt.tokens import AccessToken
+from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth import get_user_model
 
 from app.models import Slideshow
 from app.serializers import SlideshowSerializer
 
+User = get_user_model()
 # This is my first consumer, which handles a basic chat WS connection
 class ChatConsumer(AsyncWebsocketConsumer):
+
+    # Timeout for authentication (seconds)
+    AUTH_TIMEOUT = 5
+
     async def connect(self):
+        # Sets the user to be anonymous and not authenticated to start with
+        self.user = AnonymousUser()
+        self.authenticated = False
+
+        # Get the room name
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"] # Scope contains (among other things) the url witht the room name parameter in it
         self.room_group_name = f"chat_{self.room_name}" # Sets the group name for the consumer
         # OBS: Group names may only contain alphanumerics (a-z, 0-9), hyphens, underscores, or periods.
@@ -21,39 +35,94 @@ class ChatConsumer(AsyncWebsocketConsumer):
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
-        # Accept the WebSocket connection (if the user needs to be authenticated, then it should happen before this, so the connection can be rejected)
+        # Accept the WebSocket connection
         await self.accept()
 
+        # Start a timer to disconnect if authentication is not received in time
+        self.auth_timer = asyncio.create_task(self.disconnect_if_not_authenticated())
+
         # Get current slideshow data by id
-        self.slideshow = await get_slideshow(self, 2)
-        print("consumer has the data: ", self.slideshow)
-        # Send current slideshow data to the user
-        await self.send(text_data=json.dumps({"current_slideshow": self.slideshow}))
+        # self.slideshow = await get_slideshow(self, 2)
+        # print("consumer has the data: ", self.slideshow)
+        # # Send current slideshow data to the user
+        # await self.send(text_data=json.dumps({"current_slideshow": self.slideshow}))
+
+    async def disconnect_if_not_authenticated(self):
+        await asyncio.sleep(self.AUTH_TIMEOUT)
+        if not self.authenticated:
+            print("self closing")
+            await self.close(code=4001)  # 4001 = custom code for auth timeout
 
 
     async def disconnect(self, close_code):
         # Leave room group
+        print("disconnecting", close_code)
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     # Receive message from the WebSocket (message sent from the user)
     async def receive(self, text_data):
         print("receive")
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
 
-        # Send message to the whole room group
-        await self.channel_layer.group_send(self.room_group_name, {"type": "chat.message", "message": message})
-        # type indicates which method should be used to receive the event. type: chat.message can be received with chat_message (dot . is being replaced with _)
+        # Messages received when user is not authenticated
+        if not self.authenticated:
+            try:
+                data = json.loads(text_data)
+                # check if the message is of authenticate type (stop users from sending before being authenticated)
+                if data.get("type") != "authenticate":
+                    await self.close(code=4002)  # 4002 = invalid first message
+                    return
 
-        # Update the database with data from the user
-        updated_slideshow_data = text_data_json["data"]
-        print(updated_slideshow_data)
-        updated_slideshow = await patch_slideshow(self, updated_slideshow_data, 2)
-        print("updated_slideshow", updated_slideshow)
-        # send new slideshow data to the whole room group
-        await self.channel_layer.group_send(self.room_group_name, {"type": "chat.slideshow", "data": updated_slideshow})
+                token_str = data.get("token")
+                if not token_str:
+                    await self.close(code=4003)  # 4003 = missing token
+                    return
+
+                user = await get_user_from_token(token_str)
+                if user is None or not user.is_authenticated:
+                    print("exception 4004 - user: ", user)
+                    await self.close(code=4004)  # 4004 = invalid token
+                    return
+                
+                # Authentication successfull
+                self.user = user
+                self.authenticated = True
+                # Cancel the auth timeout task
+                self.auth_timer.cancel()
+
+                # Send mesage to client about succesfull authentication
+                await self.send(json.dumps({"type": "authenticated"}))
+
+            except json.JSONDecodeError:
+                await self.close(code=4005)  # 4005 = invalid JSON
+            except Exception as e:
+                print(e)
+                await self.close(code=4006)  # 4006 = generic error
+
+            return
         
+        # If already authenticated, then handle normal messages
+        await self.handle_authenticated_message(text_data)
+        
+    async def handle_authenticated_message(self, text_data):
+        """
+        Handle messages from authenticated users.
+        """
+        text_data_json = json.loads(text_data)
 
+        if text_data_json["type"] == "message":
+            message = text_data_json["message"]
+            # Send message to the whole room group
+            await self.channel_layer.group_send(self.room_group_name, {"type": "chat.message", "message": message})
+            # type indicates which method should be used to receive the event. type: chat.message can be received with chat_message (dot . is being replaced with _)
+
+        if text_data_json["type"] == "update":
+            # Update the database with data from the user
+            updated_slideshow_data = text_data_json["data"]
+            print(updated_slideshow_data)
+            updated_slideshow = await patch_slideshow(self, updated_slideshow_data, 2)
+            print("updated_slideshow", updated_slideshow)
+            # send new slideshow data to the whole room group
+            await self.channel_layer.group_send(self.room_group_name, {"type": "chat.slideshow", "data": updated_slideshow})
     
     # Receive message from the room group (messages from other users in the room)
     async def chat_message(self, event):
@@ -70,9 +139,28 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.send(text_data=json.dumps({"data": data}))
 
+
 ###############################################################################
 # Database helper functions using Decorators
 ###############################################################################
+
+@database_sync_to_async
+def get_user_from_token(token_str):
+    """
+    Uses SimpleJWT AccessToken to validate token and return User.
+    Returns None if token is invalid or expired.
+
+    :param token_str: The token from the user
+    """
+    try:
+        print("get_user_from_token starting")
+        token = AccessToken(token_str)
+        user_id = token["user_id"]
+        print("get_user_from_token - Token: ",token, "user_id", user_id)
+        return User.objects.get(id=user_id) # this fails
+    except Exception:
+        print("get_user_from_token - Exception")
+        return None
 
 @database_sync_to_async
 def get_slideshow(self, id):
