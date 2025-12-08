ff6021c1e279c22f761c82c9eacd10949e0db3cb
Freja Fischer <frej657e@stud.kea.dk>
2025-12-08 14:23:49 +0100

    Made WS js file test sending updates to socket

--- frontend/src/assets/pages/ws/main.js
+++ frontend/src/assets/pages/ws/main.js
@@ -7,7 +7,9 @@ document.addEventListener("DOMContentLoaded", async () => {
   const roomName = "room1";
   const slideshow_id = 2;
 
-  const chatSocket = new WebSocket("ws://localhost:8000" + "/ws/slideshows/" + slideshow_id + "/?branch=15");
+  const chatSocket = new WebSocket(
+    "ws://localhost:8000" + "/ws/slideshows/" + slideshow_id + "/?branch=15",
+  );
   // const chatSocket = new WebSocket("ws://localhost:8000" + "/ws/chat/" + roomName + "/");
   // ${BASE_URL}/ws/chat/{roomName}/ (BASE_URL = http://) - TO DO: Make WS BASE_URL version?
   // ws://localhost:8000/ws/chat/{roomName}/
@@ -44,7 +46,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         JSON.stringify({
           type: "authenticate",
           token: localStorage.getItem("accessToken"),
-        })
+        }),
       );
     };
   }
@@ -52,7 +54,6 @@ document.addEventListener("DOMContentLoaded", async () => {
   chatSocket.onmessage = function (e) {
     console.log("onMessage", e);
     const data = JSON.parse(e.data);
-    console.log(data);
     if (data.message) {
       document.querySelector("#chat-log").value += data.message + "\n";
     }
@@ -85,18 +86,75 @@ document.addEventListener("DOMContentLoaded", async () => {
   document.querySelector("#chat-message-submit").onclick = function (e) {
     const messageInputDom = document.querySelector("#chat-message-input");
     const message = messageInputDom.value;
-    //const updated_slideshow_data = { name: "my new name" };
+    // const updated_slideshow_data = { name: "test 123" };
+    // const updated_slideshow_data = { name: "New name", wrong_field: "test 123" };
+    // const updated_slideshow_data = { wrong_field: "test 123" };
+    const updated_slideshow_data = {
+      id: 2,
+      name: "slideshow name 3",
+      category: null,
+      tags: [],
+      mode: "slideshow",
+      branch: 15,
+      created_by: 32,
+      previewWidth: 1920,
+      previewHeight: 1080,
+      isCustomDimensions: true,
+      slideshow_data: {
+        slides: [
+          {
+            id: 1,
+            name: "nutella",
+            duration: 5,
+            elements: [
+              {
+                id: 1,
+                text: '<p><span style="font-size: 2.58cqw; line-height: 1.2; color: rgb(0, 0, 0); font-family: Roboto;">Double click to edit text</span></p>',
+                type: "tiptap-textbox",
+                gridX: 10,
+                gridY: 10,
+                border: false,
+                zIndex: 1,
+                fontSize: "12",
+                isHidden: false,
+                isLocked: false,
+                gridWidth: 110,
+                textAlign: "left",
+                textColor: "#000000",
+                fontFamily: "Roboto",
+                gridHeight: 35,
+                lineHeight: "1.2",
+                isPersistent: false,
+                tiptapContent:
+                  '<p><span style="font-size: 2.58cqw; line-height: 1.2; color: rgb(0, 0, 0); font-family: Roboto;">Double click to edit text</span></p>',
+                backgroundColor: "transparent",
+                originSlideIndex: 0,
+              },
+            ],
+            redoStack: [],
+            undoStack: [],
+            activationDate: null,
+            backgroundColor: "#ffffff",
+            deactivationDate: null,
+            activationEnabled: false,
+          },
+        ],
+      },
+      aspect_ratio: "16:9",
+    };
+    // const updated_slideshow_data = "hej";
+
+    // chatSocket.send(updated_slideshow_data); // Invalid json send
     chatSocket.send(
       JSON.stringify({
-        type: "message",
-        message: message,
-      })
+        type: "update",
+        data: updated_slideshow_data,
+      }),
     );
     // chatSocket.send(
     //   JSON.stringify({
     //     type: "update",
     //     message: message,
-    //     data: updated_slideshow_data,
     //   })
     // );
     messageInputDom.value = "";
